a6e4e922e409af1a82d68d0eff90ce9c4e7e6699
Danil <131665618+defau1tusername@users.noreply.github.com>
2023-05-02 05:21:55 +0300

    Add files via upload

---
+++ Vacation Provider/VacationPeriod.cs
@@ -0,0 +1,16 @@
+﻿public class VacationPeriod
+{
+    public DateTime Start { get; set; }
+    public DateTime End { get; set; }
+
+    public VacationPeriod(DateTime start, DateTime end)
+    {
+        if (end < start) throw new ArgumentException("Конец периода не может быть раньше начала периода");
+        Start = start;
+        End = end;
+    }
+
+    public bool IsIntersectsWith(VacationPeriod other) =>
+        Start <= other.End && other.Start <= End;  
+}
+
---
+++ Vacation Provider/VacationPeriodExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class VacationPeriodExtensions
+{
+    public static bool IsAnyIntersect(this VacationPeriod currentVacation, List<VacationPeriod> othersVacations) =>
+        othersVacations.Any(vacation => vacation.IsIntersectsWith(currentVacation));
+
+    public static bool IsMonthApart(this VacationPeriod period, VacationPeriod otherPeriod) =>
+        period.Start < otherPeriod.Start
+        ? otherPeriod.Start > period.End.AddMonths(1)
+        : period.Start > otherPeriod.End.AddMonths(1);
+}
+
